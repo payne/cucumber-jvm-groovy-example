780ff819812feb9321fdcf5db3f316c4e7a51fb0
Matt Payne <Payne@MattPayne.org>
2014-01-29 10:49:20 -0600

    Added html report and a json report.

--- src/test/java/calc/RunCukesTest.java
+++ src/test/java/calc/RunCukesTest.java
@@ -1,7 +1,8 @@
 package calc;
 
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(features = "src/test/resources/calc")
